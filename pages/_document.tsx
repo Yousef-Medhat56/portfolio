34a75a6fac221ac1fc06c1abbf44df4245711425
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-10-24 18:41:27 +0200

    add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,39 @@
-import { ColorModeScript,ColorModeScriptProps } from '@chakra-ui/react'
-import { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../utils/theme.util'
+import { ColorModeScript } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
+import theme from "../utils/theme.util";
 
 export default function Document() {
     return (
-      <Html lang='en'>
-        <Head />
-        <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+        <Html lang="en">
+            <Head>
+                {/* Fonts */}
+                {/* Agutina Font*/}
+                <link
+                    rel="preload"
+                    href="/fonts/Agutina 400.otf"
+                    as="font"
+                    type="font/otf"
+                    crossOrigin=""
+                />
+                {/* Rubik Font*/}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin=""
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body>
+                <ColorModeScript
+                    initialColorMode={theme.config.initialColorMode}
+                />
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
