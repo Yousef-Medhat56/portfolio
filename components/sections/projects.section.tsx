2cd8c3c3456cd6284d7d8ab59f3c0c9501015841
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-10-25 00:03:44 +0200

    refactor: organize images folder

--- components/sections/projects.section.tsx
+++ components/sections/projects.section.tsx
@@ -31,26 +31,26 @@ const projects: ProjectCardProps[] = [
     name: "My Portfolio",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim",
-    imgSrc: "/images/portfolio.png",
+    imgSrc: "/images/projects/portfolio.png",
     codeUrl: "https://github.com/Yousef-Medhat56/portfolio",
   },
   {
     name: "Arica Group CO. website",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim",
-    imgSrc: "/images/arica-group.png",
+    imgSrc: "/images/projects/arica-group.png",
   },
   {
     name: "MUN Event website",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim",
-    imgSrc: "/images/kaptanimun.png",
+    imgSrc: "/images/projects/kaptanimun.png",
   },
   {
     name: "Landing Page",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim",
-    imgSrc: "/images/landing-page.png",
+    imgSrc: "/images/projects/landing-page.png",
     codeUrl: "https://github.com/Yousef-Medhat56/HTML-CSS-Template-3",
     siteUrl: "https://yousef-medhat56.github.io/HTML-CSS-Template-3/",
   },
